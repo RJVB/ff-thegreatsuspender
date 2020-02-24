69777383dd47b2c92f0804c85f75604ff26c246a
Dmitry Valter <dvalter@protonmail.com>
2020-02-24 13:57:13 +0300

    Version bump (v7.1.4d)

--- pack_xpi.sh
+++ pack_xpi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p build/xpi
 cd src
-find -not -name 'html2canvas*' -not -name 'Thumbs.db' | xargs zip -q ../build/xpi/tgs-firefox.xpi 
+find -not -name 'html2canvas.min*' -not -name 'Thumbs.db' | xargs zip -q ../build/xpi/tgs-firefox.xpi 
