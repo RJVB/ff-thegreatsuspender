29a979999f61527d6cececb5fa3e26ae7319991c
René Bertin <rjvbertin@gmail.com>
2024-11-13 16:09:44 +0100

    housekeeping updates
    Committed from host : Bola

--- pack_xpi.sh
+++ pack_xpi.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+here="`dirname $0`"
+
+if [ "${here}" != "" ] ;then
+	cd ${here}
+fi
+
 mkdir -p build/xpi
 cd src
 find -not -name 'html2canvas.min*' -not -name 'Thumbs.db' | xargs zip -q ../build/xpi/tgs-firefox.xpi 
